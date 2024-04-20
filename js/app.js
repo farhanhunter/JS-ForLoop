e5041677a08ad73f10150902947f91eb5f363652
farhanhunter <96875698+farhanhunter@users.noreply.github.com>
2024-04-20 23:07:54 +0700

    ForLoop With Array

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
-for (let index = 20; index >= 0; index--) {
-  console.log("Countdown");
-  console.log(index);
-}
+// for (let index = 20; index >= 0; index--) {
+//   console.log("Countdown");
+//   console.log(index);
+// }
 
 //Dont Try Do This at home
 // for (let i = 20; i >= 0; i++) {
 //   console.log(i);
 // }
+
+const game = [
+  "Valorant",
+  "Counter Strike2",
+  "Apex Legends",
+  "GTA V",
+  "EA SPORTS 2024",
+];
+for (let index = 0; index < game.length; index++) {
+  const newGame = game[index];
+  console.log(index, newGame);
+}
