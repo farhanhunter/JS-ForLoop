f1e78f533433cbb1d85c8e932d725b012f20f414
farhanhunter <96875698+farhanhunter@users.noreply.github.com>
2024-04-20 21:42:47 +0700

    dont try at home

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@ for (let index = 20; index >= 0; index--) {
   console.log("Countdown");
   console.log(index);
 }
+
+//Dont Try Do This at home
+// for (let i = 20; i >= 0; i++) {
+//   console.log(i);
+// }
